c07da10a6d895b3dcd7e5e0d5e529d0dff71ccb7
yangweibin <wbyang@fengyuntec.com>
2020-02-23 18:18:27 +0800

    添加vue.config.js配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,72 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
+    es6: true
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-alert': 0, //禁止使用alert confirm prompt
+    'no-array-constructor': 2, //禁止使用数组构造器
+    'no-catch-shadow': 2, //禁止catch子句参数与外部作用域变量同名
+    'no-class-assign': 2, //禁止给类赋值
+    'no-cond-assign': 2, //禁止在条件表达式中使用赋值语句
+    'no-const-assign': 2, //禁止修改const声明的变量
+    'no-constant-condition': 2, //禁止在条件中使用常量表达式 if(true) if(1)
+    'no-continue': 0, //禁止使用continue
+    'no-dupe-keys': 2, //在创建对象字面量时不允许键重复 {a:1,a:1}
+    'no-dupe-args': 2, //函数参数不能重复
+    'no-duplicate-case': 2, //switch中的case标签不能重复
+    'no-else-return': 2, //如果if语句里面有return,后面不能跟else语句
+    'no-empty': 2, //块语句中的内容不能为空
+    'no-empty-character-class': 2, //正则表达式中的[]内容不能为空
+    'no-eq-null': 2, //禁止对null使用==或!=运算符
+    'no-eval': 1, //禁止使用eval
+    'no-ex-assign': 2, //禁止给catch语句中的异常参数赋值
+    'array-bracket-spacing': [2, 'never'], //是否允许非空数组里面有多余的空格
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ], //引号类型 `` "" ''
+    'semi': [2, 'never'], //语句强制分号结尾
+    'space-before-function-paren': [2, 'never'],
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': 'error'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
       }
     }
   ]
-};
+}
---
+++ vue.config.js
@@ -0,0 +1,80 @@
+const path = require('path')
+module.exports = {
+  publicPath: '/',
+  filenameHashing: true,
+  lintOnSave: process.env.NODE_ENV === 'development',
+  devServer: {
+    port: 8080,
+    open: 'Chrome', //boolean | string 是否自动打开指定或者默认浏览器
+    compress: false, //是否启用gzip压缩
+    overlay: {
+      //是否显示错误或者警告全屏覆盖层
+      warnings: false,
+      errors: true
+    }
+  },
+  pwa: {
+    // name: 'vue-admin'
+    // workboxPluginMode: 'InjectMainfest',
+    // workboxOptions: {
+    //   swSrc: path.resolve(__dirname, 'src/pwa/service-worker.js')
+    // }
+  },
+  pluginOptions: {
+    'style-resource-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/styles/_variables.scss'),
+        path.resolve(__dirname, 'src/styles/_mixins.scss')
+      ]
+    }
+  },
+  chainWebpack: config => {
+    // 设置全局 快捷路径或方法名 config.resolve.alias.set('~', path.resolve(__dirname, './src')).set('$','jquery/dist/jquery.min.js')
+    config.resolve.alias.set('~', path.resolve(__dirname, './src'))
+    config.when(process.env.NODE_ENV === 'development', config =>
+      config.devtool('cheap-eval-source-map')
+    )
+    // config.plugin('workbox')
+    config.plugins.delete('progress')
+    config
+      .plugin('simple-progress-webpack-plugin')
+      .use(require.resolve('simple-progress-webpack-plugin'), [
+        {
+          format: 'compact'
+        }
+      ])
+    config.plugins.delete('prefetch')
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.optimization.splitChunks({
+        /*chunks:
+         *all: 不管文件是动态还是非动态载入，统一将文件分离。当页面首次载入会引入所有的包
+         *async： 将异步加载的文件分离，首次一般不引入，到需要异步引入的组件才会引入。
+         *initial：将异步和非异步的文件分离，如果一个文件被异步引入也被非异步引入，那它会被打包两次（注意和all区别），用于分离页面首次需要加载的包
+         */
+        chunks: 'all',
+        cacheGroups: {
+          libs: {
+            name: 'chunk-libs',
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10,
+            chunks: 'initial' // only package third parties that are initially dependent
+          },
+          elementUI: {
+            name: 'chunk-elementUI', //分离ui框架 split elementUI into a single package
+            priority: 20, // 权重 the weight needs to be larger than libs and app or it will be packaged into libs or app
+            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+          },
+          commons: {
+            name: 'chunk-commons',
+            test: path.resolve(__dirname, 'src/components'),
+            minChunks: 3, //  minimum common number
+            priority: 5,
+            reuseExistingChunk: true
+          }
+        }
+      })
+      config.optimization.runtimeChunk('single')
+    })
+  }
+}
