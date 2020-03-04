4c0dca80efc2eead39b93a4a7b421a71e850106f
杨伟彬 <578870507@qq.com>
2020-03-04 23:31:19 +0800

    添加BasicLayout

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import ElementUI from 'element-ui'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './pwa/registerServiceWorker'
+
+Vue.use(ElementUI, {
+  size: 'medium'
+})
 
 Vue.config.productionTip = false
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import BasicLayout from '@/layouts/BasicLayout/index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    //webpackPrefetch 是否开启预加载组件
-    component: () => import(/* webpackChunkName: "about" */ /* webpackPrefetch: false  */ '../views/About.vue')
+    component: BasicLayout
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   //webpackPrefetch 是否开启预加载组件
+  //   component: () => import(/* webpackChunkName: "about" */ /* webpackPrefetch: false  */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
