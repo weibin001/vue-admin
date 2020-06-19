0c785881c367f0b2e5b44eb0cfaf30065f91524f
杨伟彬 <578870507@qq.com>
2020-06-20 01:29:14 +0800

    :fire: 移除Vuecli初始文件 Home About

---
+++ src/views/table/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+@Component({
+  name: ''
+})
+export default class extends Vue {}
+</script>
+<style lang="scss" scoped></style>
