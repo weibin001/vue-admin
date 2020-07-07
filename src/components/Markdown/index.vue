9cfd13d7c16086c65191a4129e220d7d297dd61e
杨伟彬 <578870507@qq.com>
2020-07-07 09:52:57 +0800

    :sparkles: 添加markdown编辑器

--- src/components/Markdown/index.vue
+++ src/components/Markdown/index.vue
@@ -1,16 +1,124 @@
 <template>
-  <div class="markdown-container">
-    TODO
-  </div>
+  <div :id="id" />
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import 'codemirror/lib/codemirror.css' // codemirror
+import 'tui-editor/dist/tui-editor.css' // editor ui
+import 'tui-editor/dist/tui-editor-contents.css' // editor content
+import defaultOptions from './default-options'
+import TuiEditor from 'tui-editor'
 @Component({
   name: 'Markdown'
 })
 export default class extends Vue {
-  // @Prop({default:''}) value:
+  @Prop({ required: true }) private value!: string
+  @Prop({ default: () => 'markdown-editor-' + +new Date() + ((Math.random() * 1000).toFixed(0) + '') })
+  private id!: string
+  @Prop({ default: () => defaultOptions }) private options!: tuiEditor.IEditorOptions
+  @Prop({ default: 'markdown' }) private mode!: string
+  @Prop({ default: '300px' }) private height!: string
+  // https://github.com/nhnent/tui.editor/tree/master/src/js/langs
+  @Prop({ default: 'en_US' }) private language!: string
+
+  private markdownEditor?: tuiEditor.Editor
+
+  get editorOptions() {
+    const options = Object.assign({}, defaultOptions, this.options)
+    options.initialEditType = this.mode
+    options.height = this.height
+    options.language = this.language
+    return options
+  }
+  @Watch('value')
+  private onValueChange(value: string, oldValue: string) {
+    if (this.markdownEditor) {
+      if (value !== oldValue && value !== this.markdownEditor.getValue()) {
+        this.markdownEditor.setValue(value)
+      }
+    }
+  }
+
+  @Watch('language')
+  private onLanguageChange() {
+    this.destroyEditor()
+    this.initEditor()
+  }
+
+  @Watch('height')
+  private onHeightChange(value: string) {
+    if (this.markdownEditor) {
+      this.markdownEditor.height(value)
+    }
+  }
+
+  @Watch('mode')
+  private onModeChange(value: string) {
+    if (this.markdownEditor) {
+      this.markdownEditor.changeMode(value)
+    }
+  }
+
+  mounted() {
+    this.initEditor()
+  }
+
+  private initEditor() {
+    const editorElement = document.getElementById(this.id)
+    if (!editorElement) return
+    this.markdownEditor = new TuiEditor({
+      el: editorElement,
+      ...this.editorOptions
+    })
+    if (this.value) {
+      this.markdownEditor.setValue(this.value)
+    }
+    this.markdownEditor.on('change', () => {
+      if (this.markdownEditor !== undefined) {
+        this.$emit('input', this.markdownEditor.getValue())
+      }
+    })
+  }
+
+  private destroyEditor() {
+    if (!this.markdownEditor) return
+    this.markdownEditor.off('change')
+    this.markdownEditor.remove()
+    this.markdownEditor = undefined
+  }
+
+  public focus() {
+    if (this.markdownEditor) {
+      this.markdownEditor.focus()
+    }
+  }
+
+  public setValue(value: string) {
+    if (this.markdownEditor) {
+      this.markdownEditor.setValue(value)
+    }
+  }
+
+  public getValue() {
+    if (this.markdownEditor) {
+      return this.markdownEditor.getValue()
+    }
+    return ''
+  }
+
+  public setHtml(value: string) {
+    if (this.markdownEditor) {
+      this.markdownEditor.setHtml(value)
+    }
+  }
+
+  public getHtml() {
+    if (this.markdownEditor) {
+      return this.markdownEditor.getHtml()
+    }
+    return ''
+  }
 }
 </script>
 
--- src/views/components/markdown.vue
+++ src/views/components/markdown.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="components-container">
-    TODO
-    <!-- <div class="editor-content" v-html="html" /> -->
+    <markdown-editor ref="markdownEditor" v-model="content" height="300px" :language="language" />
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { AppModule } from '@/store/modules/app'
+import MarkdownEditor from '@/components/Markdown/index.vue'
 @Component({
-  name: 'MarkdownDemo'
+  name: 'MarkdownDemo',
+  components: {
+    MarkdownEditor
+  }
 })
 export default class extends Vue {
+  private content = ''
   private html = ''
+
+  private languageTypeList: { [key: string]: string } = {
+    en: 'en_US',
+    zh: 'zh_CN'
+  }
+
+  get language() {
+    return this.languageTypeList[AppModule.language]
+  }
 }
 </script>
 
